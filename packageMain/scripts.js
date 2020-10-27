6323c0b1677f59fe2d93a844ee7d8a9467e1e5d0
diogocsouza <diogocsouza@users.noreply.github.com>
2020-10-26 22:51:32 -0300

    Complete
    PokeAPI usando HTML, CSS e JavaScript

---
+++ packageMain/scripts.js
@@ -0,0 +1,37 @@
+//funcao que irá resgatar todos pokemons da api
+const buscarPokemon = () => {
+
+    const promises = []
+
+    for (let i = 1; i <= 151; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+        promises.push(fetch(url).then((res) => res.json()))
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            nome: result.name,
+            imagem: result.sprites['front_default']
+        }))
+        mostrarPokemon(pokemon);
+    });
+
+}
+
+buscarPokemon()
+
+const mostrarPokemon = (pokemon) => {
+    console.log(pokemon);
+    const html = pokemon
+        .map(
+            (cadaPokemon) => `
+        <li class = "cartao">
+            <h2>${cadaPokemon.nome}</h2>
+            <img src = "${cadaPokemon.imagem}"/>
+        </li>
+        `).join('');
+    pokedex.innerHTML = html;
+};
+
+
+
+const pokedex = document.getElementById('pokedex');
